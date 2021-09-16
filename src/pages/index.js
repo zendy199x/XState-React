a80d7d3df4103b5539209d23539b9b8d81e1e2d6
Zendy <nguyenduc.051194@gmail.com>
2021-09-16 16:33:24 +0700

    Handle current stats api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import { useMachine } from "@xstate/react";
+import React from "react";
+import { statsMachine } from "../xstore/startsMachine";
 
+const App = () => {
+  const [current, send] = useMachine(statsMachine);
 
-const App = () => (
-  <div>CoronaVirus Information</div>
-)
+  return (
+    <>
+      <div>CoronaVirus Information</div>
+      {current.matches("fetchStats") && <div>Loading Stats…</div>}
+      {current.matches("error") && <div>Error fetching stats…</div>}
+      {current.matches("ready") && <div>Stats loaded!</div>}
+    </>
+  );
+};
 
-export default App
+export default App;
